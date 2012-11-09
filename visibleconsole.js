58c224fddcb32598b045723c786c0f07f2975408
James Tomasino <james@tomasino.org>
2012-11-09 12:37:27 -0500

    renaming ids with visibleconsole

--- visibleconsole.js
+++ visibleconsole.js
@@ -18,19 +18,19 @@
 			VisibleConsole._isEnabled = true;
 
 			// Prepare #console element
-			VisibleConsole.consoleEl = document.getElementById('console');
+			VisibleConsole.consoleEl = document.getElementById('visibleconsole');
 			if (!VisibleConsole.consoleEl)  {
 				VisibleConsole.consoleEl = document.createElement('div');
-				VisibleConsole.consoleEl.id = 'console';
+				VisibleConsole.consoleEl.id = 'visibleconsole';
 				document.body.appendChild(VisibleConsole.consoleEl);
 			}
 
 			// Prepare fallback console
-			VisibleConsole.fallBackIFrame = document.getElementById('consoleiframe');
+			VisibleConsole.fallBackIFrame = document.getElementById('visibleconsoleiframe');
 			if ( !VisibleConsole.fallBackIFrame) {
 				VisibleConsole.fallBackIFrame = document.createElement('iframe');
 				VisibleConsole.fallBackIFrame.style.display = 'none';
-				VisibleConsole.fallBackIFrame.id = 'consoleiframe';
+				VisibleConsole.fallBackIFrame.id = 'visibleconsoleiframe';
 				document.body.appendChild(VisibleConsole.fallBackIFrame);
 			}
 
@@ -40,7 +40,7 @@
 			// Fix native code interpolation error on apply: http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
 			VisibleConsole.browserLog = Function.prototype.bind.call(VisibleConsole.browserVisibleConsole.log, VisibleConsole.browserVisibleConsole);
 
-			// Ouput to #console
+			// Ouput to #visibleconsole
 			window.console = {
 				log: function () {
 
