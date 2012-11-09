a2465adf62e97c08b7cbbb51e879a66fa1aebf62
James Tomasino <james@tomasino.org>
2012-11-09 12:11:59 -0500

    adding initial version

---
+++ visibleconsole.js
@@ -0,0 +1,86 @@
+(function(window, document){
+	"use strict";
+
+	function VisibleConsole () {}
+
+	// Static properties
+	VisibleConsole.browserVisibleConsole;
+	VisibleConsole.browserLog;
+	VisibleConsole.consoleEl;
+	VisibleConsole.fallBackIFrame;
+
+	// Static psudo-privates
+	VisibleConsole._isEnabled = false;
+
+	VisibleConsole.enable = function () {
+
+		if (VisibleConsole._isEnabled === false) {
+			VisibleConsole._isEnabled = true;
+
+			// Prepare #console element
+			VisibleConsole.consoleEl = document.getElementById('console');
+			if (!VisibleConsole.consoleEl)  {
+				VisibleConsole.consoleEl = document.createElement('div');
+				VisibleConsole.consoleEl.id = 'console';
+				document.body.appendChild(VisibleConsole.consoleEl);
+			}
+
+			// Prepare fallback console
+			VisibleConsole.fallBackIFrame = document.getElementById('consoleiframe');
+			if ( !VisibleConsole.fallBackIFrame) {
+				VisibleConsole.fallBackIFrame = document.createElement('iframe');
+				VisibleConsole.fallBackIFrame.style.display = 'none';
+				VisibleConsole.fallBackIFrame.id = 'consoleiframe';
+				document.body.appendChild(VisibleConsole.fallBackIFrame);
+			}
+
+			// Prepare browserVisibleConsole
+			VisibleConsole.browserVisibleConsole = VisibleConsole.fallBackIFrame.contentWindow.console;
+
+			// Fix native code interpolation error on apply: http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
+			VisibleConsole.browserLog = Function.prototype.bind.call(VisibleConsole.browserVisibleConsole.log, VisibleConsole.browserVisibleConsole);
+
+			// Ouput to #console
+			window.console = {
+				log: function () {
+
+					var output = "";
+
+					for (var i = 0; i < arguments.length; ++i)
+					{
+						if (typeof(arguments[i]) == 'string') {
+							output += arguments[i];
+							if (i < arguments.length - 1) output += ' ';
+							else output += "\n";
+						}
+					}
+
+					VisibleConsole.consoleEl.innerHTML += output;
+
+					// Output to native console
+					if (VisibleConsole.browserVisibleConsole && VisibleConsole.browserVisibleConsole.log ) {
+						VisibleConsole.browserLog.apply ( null, arguments);
+					}
+
+				}
+			}
+		}
+	}
+
+	VisibleConsole.disable = function () {
+		if (VisibleConsole._isEnabled === true) {
+			VisibleConsole._isEnabled = false;
+
+			window.console = VisibleConsole.browserVisibleConsole;
+			VisibleConsole.consoleEl.parentNode.removeChild(VisibleConsole.consoleEl);
+			VisibleConsole.fallBackIFrame.parentNode.removeChild(VisibleConsole.fallBackIFrame);
+			VisibleConsole.consoleEl = null;
+			VisibleConsole.fallBackIFrame = null;
+			VisibleConsole.browserLog = null;
+		}
+	}
+
+	window.VisibleConsole = VisibleConsole;
+
+})(window, document);
+
