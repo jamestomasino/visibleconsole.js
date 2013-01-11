0f8b2cca4fcd40ae47305082384efc79b06a21a7
Eric Emmons <eric.emmons@gmail.com>
2013-01-11 15:40:44 -0500

    renamed stlyle.css to visibleconsole.css. Removed 'resize: both;' added code to dynamically draw resize handle.

--- visibleconsole.js
+++ visibleconsole.js
@@ -80,6 +80,8 @@
 				VisibleConsole.handleEl.id = 'visibleconsolehandle';
 				VisibleConsole.handleEl.onmousedown = VisibleConsole._startResizing;
 				document.getElementById('visibleconsole').appendChild(VisibleConsole.handleEl);
+				VisibleConsole.handleEl.appendChild(VisibleConsole.createline(21, 15, 16, 20));
+				VisibleConsole.handleEl.appendChild(VisibleConsole.createline(21, 9, 10, 20));
 			}
 
 			// Prepare fallback console
@@ -156,6 +158,38 @@
 			VisibleConsole.fallBackIFrame = null;
 			VisibleConsole.browserLog = null;
 		}
+	};
+
+	VisibleConsole.createline = function (x1, y1, x2, y2)	{
+
+		var isIE = navigator.userAgent.indexOf("MSIE") > -1;
+
+		if (x2 < x1) {
+			var temp = x1;
+			x1 = x2;
+			x2 = temp;
+			temp = y1;
+			y1 = y2;
+			y2 = temp;
+		}
+		var line = document.createElement("div");
+		line.className = "visibleconsoleline";
+		var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+		line.style.width = length + "px";
+
+		if (isIE) {
+			line.style.top = (y2 > y1) ? y1 + "px" : y2 + "px";
+			line.style.left = x1 + "px";
+			var nCos = (x2-x1)/length;
+			var nSin = (y2-y1)/length;
+			line.style.filter = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=" + nCos + ", M12=" + -1*nSin + ", M21=" + nSin + ", M22=" + nCos + ")";
+		} else {
+			var angle = Math.atan((y2-y1)/(x2-x1));
+			line.style.top = y1 + 0.5*length*Math.sin(angle) + "px";
+			line.style.left = x1 - 0.5*length*(1 - Math.cos(angle)) + "px";
+			line.style.MozTransform = line.style.WebkitTransform = line.style.OTransform= "rotate(" + angle + "rad)";
+		}
+		return line;
 	};
 
 	VisibleConsole._stop = function () {
